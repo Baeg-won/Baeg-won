b4e592d411e00c6d8b31d42f74051f7e3c5608f5
Baeg-won <45421117+Baeg-won@users.noreply.github.com>
2023-05-31 17:59:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ markdown_text = """
 <img src="https://img.shields.io/badge/JAVA-007396?style=for-the-badge&logo=Java&logoColor=white">
 <img src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=Spring&logoColor=white">
 <img src="https://img.shields.io/badge/MySQL-4479A1?style=for-the-badge&logo=MySQL&logoColor=white">
-<img src="https://img.shields.io/badge/Oracle-F80000?style=for-the-badge&logo=Oracle&logoColor=white"> 
 
 <br/>
 <br/>
