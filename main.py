b6277f3677c126efe814730b5587a3800c470177
Baeg-won <45421117+Baeg-won@users.noreply.github.com>
2023-05-31 17:57:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ markdown_text = """
 
 #### :pencil: Study Log
 
-[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Baeg-won&layout=compact&show_icons=true&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Baeg-won&layout=compact&show_icons=true)](https://github.com/anuraghazra/github-readme-stats)
 
 </div>
 
