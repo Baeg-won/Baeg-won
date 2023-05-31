584fc74199a1324a9bb3b61587ab0526695fc1c3
Baeg-won <45421117+Baeg-won@users.noreply.github.com>
2023-05-31 18:09:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ markdown_text = """
 ####  :wave: Welcome my github profile !
   
 <br/>
+
+####  :star: Tech & Daily Blog
+<img src="https://img.shields.io/badge/tistory-000000?style=flat-square&logo=Blog&logoColor=white"/>
+
+<br/>
+<br/>
   
 ####  :clipboard: Once I've Used
-
 <img src="https://img.shields.io/badge/JAVA-007396?style=for-the-badge&logo=Java&logoColor=white">
 <img src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=Spring&logoColor=white">
 <img src="https://img.shields.io/badge/MySQL-4479A1?style=for-the-badge&logo=MySQL&logoColor=white">
@@ -23,7 +28,6 @@ markdown_text = """
 <br/>
 
 #### :pencil: Study Log
-
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Baeg-won&layout=compact&show_icons=true)](https://github.com/anuraghazra/github-readme-stats)
 
 </div>
